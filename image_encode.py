1e5fc954995ce5e30343d4f6bf9405fe137d58a7
Tran Diu Huyen <22127170@student.hcmus.edu.vn>
2025-12-05 08:11:03 +0700

    sắp xong p1

--- image_encode.py
+++ image_encode.py
@@ -1,6 +1,10 @@
 import base64
 
-with open("autoencoder.jpg", "rb") as f:
+input_file = "autoencoder.png" # Sửa tên file image nếu cần
+output_file = "autoencoder_base64.txt"
+
+with open(input_file, "rb") as f: 
     data = base64.b64encode(f.read()).decode()
 
-print(data) 
+with open(output_file, "w") as out:
+    out.write(data)
